43552c8a9060f313998958e38a67f8f41826182f
Tim Moran <tim@t-moran.com>
2013-12-14 18:11:43 +0000

    Expand selectors properly

--- src/main/java/mrtim/sasscompiler/ExpansionVisitor.java
+++ src/main/java/mrtim/sasscompiler/ExpansionVisitor.java
@@ -1,20 +1,25 @@
 package mrtim.sasscompiler;
 
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
 import mrtim.sasscompiler.grammar.SassBaseVisitor;
 import mrtim.sasscompiler.grammar.SassParser;
 import mrtim.sasscompiler.grammar.SassParser.Sass_fileContext;
+import mrtim.sasscompiler.grammar.SassParser.Selector_combinationContext;
 import mrtim.sasscompiler.grammar.SassParser.ValueContext;
 import mrtim.sasscompiler.grammar.SassParser.Variable_defContext;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class ExpansionVisitor extends SassBaseVisitor<Void> {
 
     private final ParseTreeProperty<String> expandedSelectors;
     private final ParseTreeProperty<String> variableValues;
-    private Stack<String> selectorStack = new Stack<>();
+    private Stack<List<Selector_combinationContext>> selectorStack = new Stack<>();
     private Stack<Scope> scopeStack = new Stack<>();
 
     public ExpansionVisitor(ParseTreeProperty<String> expandedSelectors, ParseTreeProperty<String> variableValues) {
@@ -38,11 +43,49 @@ public class ExpansionVisitor extends SassBaseVisitor<Void> {
     @Override
     public Void visitRuleset(SassParser.RulesetContext ctx) {
         SassParser.Selector_listContext selector = ctx.selector_list();
-        selectorStack.push(selector.getText());
-        expandedSelectors.put(selector, StringUtils.join(selectorStack, " "));
+        selectorStack.push(ctx.selector_list().selector_combination());
+        expandedSelectors.put(selector, expandSelectorStack());
         visitChildren(ctx);
         selectorStack.pop();
         return null;
+    }
+
+    private String expandSelectorStack() {
+        Stack<List<Selector_combinationContext>> selectors = new Stack<>();
+        selectors.addAll(selectorStack);
+
+        List<String> expanded = new ArrayList<>();
+
+        while (!selectors.empty()) {
+            List<String> newPrefixes = extractSelectors(selectors.pop());
+            if (expanded.isEmpty()) {
+                expanded = newPrefixes;
+            }
+            else {
+                expanded = addPrefixes(newPrefixes, expanded);
+            }
+        }
+
+        return StringUtils.join(expanded, ", ");
+    }
+
+    private List<String> addPrefixes(List<String> newPrefixes, List<String> items) {
+        List<String> prefixed = new ArrayList<>();
+        for (String newPrefix: newPrefixes) {
+            for (String item: items) {
+                prefixed.add(newPrefix + " " + item);
+            }
+        }
+        return prefixed;
+    }
+
+    private List<String> extractSelectors(List<Selector_combinationContext> selectorCombinations) {
+        return FluentIterable.from(selectorCombinations).transform(new Function<Selector_combinationContext, String>() {
+            @Override
+            public String apply(Selector_combinationContext input) {
+                return input.getText();
+            }
+        }).toList();
     }
 
     @Override
