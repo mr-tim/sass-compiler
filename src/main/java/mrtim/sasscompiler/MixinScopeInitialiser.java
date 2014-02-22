e947416f7f48300922c0732fe1533792db0b5ee4
Tim Moran <tim@t-moran.com>
2014-02-22 17:38:56 +0000

    Deal with mixing parameters and defaults correctly

--- src/main/java/mrtim/sasscompiler/MixinScopeInitialiser.java
+++ src/main/java/mrtim/sasscompiler/MixinScopeInitialiser.java
@@ -1,7 +1,11 @@
 package mrtim.sasscompiler;
 
+import mrtim.sasscompiler.expr.ExpressionValue;
 import mrtim.sasscompiler.expr.StringExpressionValue;
 import mrtim.sasscompiler.grammar.SassParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.*;
 
 public class MixinScopeInitialiser {
 
@@ -15,8 +19,66 @@ public class MixinScopeInitialiser {
     }
 
     public void initialiseScope(Scope scope) {
-        for (SassParser.Variable_defContext variableDef: mixinDefParameters.variable_def()) {
-            scope.define(variableDef.VARIABLE().getText(), new StringExpressionValue("xxx"));
+        List<String> paramNames = positionalParamNames(mixinDefParameters.variable_def());
+
+        int positionalIndex = 0;
+
+        Map<String, ExpressionValue> evaluatedParams = new HashMap<>();
+        if (includeParameters != null) {
+            for (SassParser.ParameterContext includeParam: includeParameters.parameter()) {
+                if (isKeywordParam(includeParam)) {
+                    String paramName = includeParam.named_parameter().VARIABLE().getText();
+                    paramNames.remove(paramName);
+                    evaluatedParams.put(paramName, extractExpressionValue(includeParam, scope));
+                }
+            }
+            for (SassParser.ParameterContext includeParam: includeParameters.parameter()) {
+                if (!isKeywordParam(includeParam)) {
+                    evaluatedParams.put(paramNames.get(positionalIndex++), extractExpressionValue(includeParam, scope));
+                }
+            }
         }
+
+        for (SassParser.Variable_defContext variableDef: mixinDefParameters.variable_def()) {
+            String variableName = variableDef.VARIABLE().getText();
+            if (!evaluatedParams.containsKey(variableName)) {
+                evaluatedParams.put(variableName, evaluateInScope(scope, variableDef.expression_list()));
+            }
+        }
+
+        for (Map.Entry<String, ExpressionValue> e: evaluatedParams.entrySet()) {
+            scope.define(e.getKey(), e.getValue());
+        }
+    }
+
+    private boolean isKeywordParam(SassParser.ParameterContext includeParam) {
+        return includeParam.named_parameter() != null;
+    }
+
+    private List<String> positionalParamNames(List<SassParser.Variable_defContext> variableDefs) {
+        List<String> paramNames = new ArrayList<>();
+        for (SassParser.Variable_defContext variableDef: variableDefs) {
+            paramNames.add(variableDef.VARIABLE().getText());
+        }
+        return paramNames;
+    }
+
+    private ExpressionValue extractExpressionValue(SassParser.ParameterContext includeParam, Scope scope) {
+        if (includeParam.IDENTIFIER() != null) {
+            return new StringExpressionValue(includeParam.IDENTIFIER().getText());
+        }
+        else if (includeParam.value() != null) {
+            return evaluateInScope(scope, includeParam.value());
+        }
+        else if (includeParam.named_parameter() != null) {
+            return evaluateInScope(scope, includeParam.named_parameter().expression_list());
+        }
+        else {
+            throw new IllegalArgumentException("Could not extract parameter value from ParameterContext: " + includeParam.getText());
+        }
+    }
+
+    private ExpressionValue evaluateInScope(Scope scope, ParseTree expression) {
+        return new ExpressionVisitor(scope).visit(expression);
     }
 }
